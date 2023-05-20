b1d4a637dd0941dcc95c4f41333e2c1af0ed803e
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-05-20 16:46:25 -0400

    add comment card component

--- src/components/CommentsList/CommentCard/CommentCard.js
+++ src/components/CommentsList/CommentCard/CommentCard.js
@@ -1,6 +1,20 @@
-import "./CommentsCard.scss";
-function CommentCard() {
-  return <div>CommentCard</div>;
+import { formatTimestamp } from "../../../utils";
+import "./CommentCard.scss";
+function CommentCard({ comment }) {
+  const date = formatTimestamp(comment.timestamp);
+  return (
+    <>
+      <div className="comment">
+        <div className="comment__avatar avatar"></div>
+        <div className="comment__container">
+          <div className="comment__name">{comment.name}</div>
+          <div className="comment__date">{date}</div>
+          <div className="comment__content">{comment.comment}</div>
+        </div>
+      </div>
+      <div className="divider"></div>
+    </>
+  );
 }
 
 export default CommentCard;
