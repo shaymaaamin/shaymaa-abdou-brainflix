20a0cc3c4c08b7b863bced1506720b87097855c0
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-05-20 16:47:14 -0400

    add comment form component

--- src/components/CommentsList/CommentsForm/CommentsForm.js
+++ src/components/CommentsList/CommentsForm/CommentsForm.js
@@ -1,6 +1,24 @@
 import "./CommentsForm.scss";
 function CommentsForm() {
-  return <div>CommentsForm</div>;
+  return (
+    <>
+      <div className="comments__form-wrapper">
+        <h3 className="comments__form-title">join the conversation</h3>
+        <div className="comments__form-avatar avatar"></div>
+        <form className="comments__form">
+          <textarea
+            name="text"
+            placeholder="Add a new comment"
+            rows="5"
+          ></textarea>
+          <button type="submit" className="icon icon--comment">
+            comment
+          </button>
+        </form>
+      </div>
+      <div className="divider"></div>
+    </>
+  );
 }
 
 export default CommentsForm;
