1194c6e21d3a03f00f810214ebf6e91d7467c950
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-05-28 01:21:28 -0400

    fix api calls for github pages

--- src/api.js
+++ src/api.js
@@ -4,5 +4,5 @@ axios.defaults.baseURL = "https://project-2-api.herokuapp.com";
 axios.defaults.params = { api_key: `84906660-7a1c-405b-a925-c0cc480ab985` };
 axios.interceptors.response.use((response) => response.data);
 
-export const getVideos = () => axios.get(`videos`);
-export const getVideoDetails = (id) => axios.get(`videos/${id}`);
+export const getVideos = () => axios.get(`/videos`);
+export const getVideoDetails = (id) => axios.get(`/videos/${id}`);
