8900017faf49dbf9010162ff5bac9f517d1a4fb2
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-05-28 16:34:29 -0400

    update global styles for consistent gutter sizes

--- src/components/Topnav/Topnav.js
+++ src/components/Topnav/Topnav.js
@@ -8,33 +8,36 @@ function Topnav() {
   const navigateTo = useNavigate();
 
   return (
-    <section className="topnav">
-      <div className="page__section">
-        <Link to="/" className="topnav__logo-link">
-          <img className="topnav__logo" src={logo} alt="Topnav Logo" />
-        </Link>
+    <>
+      <section className="topnav">
+        <div className="topnav__wrapper">
+          <Link to="/" className="topnav__logo-link">
+            <img className="topnav__logo" src={logo} alt="Topnav Logo" />
+          </Link>
 
-        <form className="topnav__form">
-          <div className="form__field">
-            <input
-              className="topnav__search icon icon--search"
-              type="text"
-              name="search"
-              placeholder="Search"
-            />
-            <div className="topnav__avatar"></div>
-          </div>
-          <button
-            className="topnav__button icon icon--upload"
-            type="button"
-            onClick={() => navigateTo("/upload")}
-          >
-            upload
-          </button>
-          <div className="topnav__avatar topnav__avatar--lg"></div>
-        </form>
-      </div>
-    </section>
+          <form className="topnav__form">
+            <div className="form__field">
+              <input
+                className="topnav__search icon icon--search"
+                type="text"
+                name="search"
+                placeholder="Search"
+              />
+              <div className="topnav__avatar"></div>
+            </div>
+            <button
+              className="topnav__button icon icon--upload"
+              type="button"
+              onClick={() => navigateTo("/upload")}
+            >
+              upload
+            </button>
+            <div className="topnav__avatar topnav__avatar--lg"></div>
+          </form>
+        </div>
+      </section>
+      <div className="divider"></div>
+    </>
   );
 }
 
