4cf5a913945231150ea6392cb2d36305e0e17235
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-06-04 01:30:08 -0400

    add endpoint for saving uploaded video details

--- api/server.js
+++ api/server.js
@@ -6,8 +6,11 @@ const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
+
+app.use("/public", express.static("public"));
 
 const videosPath = "./data/videos.json";
 const videoDetailsPath = "./data/video-details.json";
@@ -22,6 +25,57 @@ const getVideoDetails = () => {
 app.get("/videos", (req, res) => {
   const videos = getVideos();
   res.json(videos);
+});
+
+function saveImageToFilesystem(imageDataUrl, name) {
+  const matches = imageDataUrl.match(/^data:(.+);base64,(.+)$/);
+  const extension = matches[1].split("/")[1];
+  const base64Data = matches[2];
+  const buffer = Buffer.from(base64Data, "base64");
+  const fileName = `public/${name}.${extension}`;
+  fs.writeFileSync(fileName, buffer);
+  return `http://localhost:${PORT}/${fileName}`;
+}
+
+app.post("/videos", (req, res) => {
+  const id = uuid.v4();
+  const title = req.body.title;
+  const description = req.body.description;
+  const timestamp = Date.now();
+  const image = saveImageToFilesystem(req.body.image, uuid.v4());
+
+  const channel = "Shaymaa Abdou";
+  const duration = "2:27";
+  const video = "http://localhost:3000/public/BrainStation Sample Video.mp4";
+
+  const videos = getVideos();
+  const videoDetails = getVideoDetails();
+
+  videos.push({
+    id,
+    title,
+    channel,
+    image,
+  });
+
+  videoDetails.push({
+    id,
+    title,
+    channel,
+    image,
+    description,
+    views: "0",
+    likes: "0",
+    duration,
+    video,
+    timestamp,
+    comments: [],
+  });
+
+  fs.writeFileSync(videosPath, JSON.stringify(videos, null, 4));
+  fs.writeFileSync(videoDetailsPath, JSON.stringify(videoDetails, null, 4));
+
+  res.status(201).json(videoDetails[videoDetails.length - 1]);
 });
 
 app.get("/videos/:videoId", (req, res) => {
