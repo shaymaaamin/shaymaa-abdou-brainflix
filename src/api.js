a921f6793ccba270027a860304b7ac3b8a904e51
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-06-04 01:29:00 -0400

    add uploadVideo api method & add catch for getVideos & getVideoDetails

--- src/api.js
+++ src/api.js
@@ -3,8 +3,15 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:3000";
 axios.interceptors.response.use((response) => response.data);
 
-export const getVideos = () => axios.get(`/videos`);
-export const getVideoDetails = (id) => axios.get(`/videos/${id}`);
+export const getVideos = () => axios.get(`/videos`).catch(() => []);
+export const getVideoDetails = (id) =>
+  axios.get(`/videos/${id}`).catch(() => false);
 
-export const addComment = (videoId, name, comment) => axios.post(`/videos/${videoId}/comments`, {name,comment});
-export const deleteComment = (videoId,commentId) =>  axios.delete(`/videos/${videoId}/comments/${commentId}`);
+export const uploadVideo = (title, description, image) =>
+  axios.post(`/videos`, { title, description, image });
+
+export const addComment = (videoId, name, comment) =>
+  axios.post(`/videos/${videoId}/comments`, { name, comment });
+
+export const deleteComment = (videoId, commentId) =>
+  axios.delete(`/videos/${videoId}/comments/${commentId}`);
